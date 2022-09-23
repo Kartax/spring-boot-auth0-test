f8932f7f3c2f59824c93909cb701565a455729b9
Philipp Cuntz <philipp.cuntz@boehringer-ingelheim.com>
2022-09-23 10:49:00 +0200

    initial commit

---
+++ src/main/java/de/kartax/springbootauth0test/SpringBootAuth0TestApplication.java
@@ -0,0 +1,13 @@
+package de.kartax.springbootauth0test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuth0TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAuth0TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kartax/springbootauth0test/SpringBootAuth0TestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kartax.springbootauth0test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuth0TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
